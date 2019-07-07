fe6e4fe0e924370827d3c74de491e19b192d23a0
bashhike <ashp.pandey916@gmail.com>
2019-07-07 18:49:26 +0530

    Initial commit. Geohash base file.
    Implemented the following functions:
    encode, decode, bbox, adjacent

---
+++ geohash-base.py
@@ -0,0 +1,126 @@
+import sys 
+
+# The 32 bit character set to be used in the geohash. 
+_base32 = '0123456789bcdefghjkmnpqrstuvwxyz'
+
+# Mapping of each base32 character to an integer. 
+_basemap = {}
+for i in range(32):
+	_basemap[_base32[i]] = i
+
+
+
+def decode(ghash, errors = False):
+	"""
+	Function to decode a geohash to lat, lon values. 
+	errors: Flag to include or exclude error parameters from the return value. 
+	"""
+	lat_range = (-90.0, 90.0)
+	lon_range = (-180.0, 180.0)
+	index = 0
+
+	for c in ghash:
+		# The binary representation of the character. 
+		c2i = _basemap[c]
+		
+		# Mask is used in order to extract the bit information from the integer. 
+		for mask in [16, 8, 4, 2, 1]:
+			if index%2:
+				# Latitude when index is odd. 
+				if mask & c2i:
+					# 1 at the position of mask 
+					lat_range = ((lat_range[0] + lat_range[1])/ 2, lat_range[1])
+				else:
+					lat_range = (lat_range[0], (lat_range[0] + lat_range[1])/ 2)
+			else:
+				# Longitude when index is even
+				if mask & c2i:
+					# 1 at the position of mask 
+					lon_range = ((lon_range[0] + lon_range[1])/ 2, lon_range[1])
+				else:
+					lon_range = (lon_range[0], (lon_range[0] + lon_range[1])/ 2)
+			index += 1
+
+	if errors:
+		return (
+			(lat_range[0] + lat_range[1])/ 2, 
+			(lon_range[0] + lon_range[1])/ 2, 
+			(lat_range[1] - lat_range[0])/ 2, 
+			(lon_range[1] - lon_range[0])/ 2)
+	else:
+		return ((lat_range[0] + lat_range[1])/ 2, (lon_range[0] + lon_range[1])/ 2)
+
+
+def encode(lat, lon, precision = 12):
+	"""
+	Function to encode a given lat, lon pair to a geohash. 
+	"""
+	lat_range = (-90.0, 90.0)
+	lon_range = (-180.0, 180.0)
+	index = 0
+	ghash = ''
+
+	for i in range(precision):
+		ch = 0
+		for mask in [16, 8, 4, 2, 1]:
+			if index%2:
+				# Latitude in case of odd index. 
+				mid_pt = (lat_range[0] + lat_range[1])/ 2
+				if lat >= mid_pt:
+					ch |= mask 
+					lat_range = (mid_pt, lat_range[1])
+				else:
+					lat_range = (lat_range[0], mid_pt)
+			else:
+				# Longitude in case of even index.
+				mid_pt = (lon_range[0] + lon_range[1])/ 2
+				if lon >= mid_pt:
+					ch |= mask 
+					lon_range = (mid_pt, lon_range[1])
+				else:
+					lon_range = (lon_range[0], mid_pt)
+			index += 1
+		ghash += _base32[ch]
+	return ghash
+
+
+def bbox(ghash):
+	"""Returns the bounding box of the geohash"""
+	lat, lon, lat_err, lon_err = decode(ghash, errors = True)
+	return {
+	'n': lat + lat_err, 
+	's': lat - lat_err, 
+	'w': lon - lon_err, 
+	'e': lon + lon_err
+	}
+
+
+def adjacent(ghash, dir):
+	"""
+	Return a adjacent geohash in the specified direction
+	dir: n, s, e, w
+
+	"""
+	# Based on https://github.com/davetroy/geohash-js
+	neighbour = {
+	    'n': ( 'p0r21436x8zb9dcf5h7kjnmqesgutwvy', 'bc01fg45238967deuvhjyznpkmstqrwx' ),
+	    's': ( '14365h7k9dcfesgujnmqp0r2twvyx8zb', '238967debc01fg45kmstqrwxuvhjyznp' ),
+	    'e': ( 'bc01fg45238967deuvhjyznpkmstqrwx', 'p0r21436x8zb9dcf5h7kjnmqesgutwvy' ),
+	    'w': ( '238967debc01fg45kmstqrwxuvhjyznp', '14365h7k9dcfesgujnmqp0r2twvyx8zb' ),
+	}
+	border = {
+	    'n': ( 'prxz',     'bcfguvyz' ),
+	    's': ( '028b',     '0145hjnp' ),
+	    'e': ( 'bcfguvyz', 'prxz'     ),
+	    'w': ( '0145hjnp', '028b'     ),
+	}
+
+	lastch = ghash[-1]
+	parent = ghash[:-1]
+	gtype = len(ghash)%2
+
+	# Check for edge cases that don't share a common prefix. 
+	if lastch in border[dir][gtype] and parent != '':
+		parent = adjacent(parent, dir)
+
+	return parent + _base32[neighbour[dir][gtype].index(lastch)]

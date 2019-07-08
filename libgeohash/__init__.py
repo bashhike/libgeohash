f85c546da1db37dcef69f4672faae34c1abc9840
bashhike <ashp.pandey916@gmail.com>
2019-07-08 09:29:59 +0530

    Add __init__.py file.

---
+++ libgeohash/__init__.py
@@ -0,0 +1,11 @@
+from .geohash_base import decode, encode, bbox, adjacent, neighbors
+from .distance_metrics import distance, dimensions
+
+__author__ = 'Abhishek Pandey'
+
+__version__ = '0.1'
+
+__all__ = {
+	'encode', 'decode', 'bbox', 'adjacent', 'neighbors', 
+	'distance', 'dimensions'
+}

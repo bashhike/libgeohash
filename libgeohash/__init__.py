4d38fcfbfc9db5b8090e1dd3a94e975279854968
bashhike <ashp.pandey916@gmail.com>
2019-07-17 00:02:45 +0530

    Add functions from .geometry.

--- libgeohash/__init__.py
+++ libgeohash/__init__.py
@@ -1,5 +1,6 @@
 from .geohash_base import decode, encode, bbox, adjacent, neighbors, expand
 from .distance_metrics import distance, dimensions
+from .geometry import geohash_to_polygon, polygon_to_geohash
 
 __author__ = 'Abhishek Pandey'
 
@@ -7,5 +8,5 @@ __version__ = '0.1'
 
 __all__ = {
 	'encode', 'decode', 'bbox', 'adjacent', 'neighbors', 
-	'distance', 'dimensions', 'expand'
+	'distance', 'dimensions', 'expand', 'geohash_to_polygon', 'polygon_to_geohash'
 }
